6e2a28ea5091a7806eec3ca67207f2b03c271b74
Jose Fernando Sanchez Gomez <josefersango@gmail.com>
2021-10-02 16:50:50 -0500

    operario

--- coldevs/src/Operarios/Operario.jsx
+++ coldevs/src/Operarios/Operario.jsx
@@ -1,20 +1,70 @@
 import React from 'react'
 import '../App.css'
 import { useHistory } from 'react-router-dom'
-import TablaVentas from './TablaVentas'
+import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const Operario = ({user}) => {
+const Operario = ({ user }) => {
 
     const history = useHistory();
 
     return (
         <>
             {
-                user.Id_Empleado === 0  ?
-                history.push('/denegado'):
-                <div className="main">
-                    <h1>Hello Operario...</h1>
-                </div>
+                user.Id_Empleado === 0 ?
+                    history.push('/denegado') :
+                    <div className="main">
+                        <div className="detalle">
+                            <div className="formulario">
+                                <h1>Productos Pendientes</h1>
+
+                            </div>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Id_Producto</th>
+                                        <th scope="col">Producto</th>
+                                        <th scope="col">Cantidad</th>
+                                        <th scope="col">Terminado</th>
+
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+
+                                        <td>Camisa</td>
+                                        <td>3</td>
+
+
+                                        <td className="detail" ><BsFillCheckCircleFill /></td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+
+                                        <td>Jean</td>
+                                        <td>5</td>
+
+
+                                        <td className="detail" ><BsFillCheckCircleFill/></td>
+
+                                    </tr>
+
+                                    <tr>
+                                        <th scope="row">3</th>
+
+                                        <td>Polo</td>
+                                        <td>2</td>
+
+
+                                        <td className="detail" ><BsFillCheckCircleFill /></td>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                        </div>
+                    </div>
             }
         </>
     )
