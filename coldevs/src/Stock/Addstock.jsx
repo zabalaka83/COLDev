aa1ab6b1111b9ac85e1ba18a01281bbfd63011e6
Jeffry Kamsteeg Rodiño <j.kamsteeg1570@pascualbravo.edu.co>
2021-10-09 04:33:45 -0500

    Creación de las vistas adiccionar y restar stock
    Falta programar el botón para que haga la resta desde la data.js quemada.

--- coldevs/src/Stock/Addstock.jsx
+++ coldevs/src/Stock/Addstock.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import '../App.css'
 import { useHistory } from 'react-router-dom'
 
-const Addstock = ({user}) => {
-    
+const Addstock = ({ user }) => {
+
     const history = useHistory();// id Nombre precio cantidad
 
 
@@ -14,6 +14,14 @@ const Addstock = ({user}) => {
                     history.push('/denegado') :
                     <div className="main">
                         <div>
+                            <div class="hstack gap-3">
+                                <h1>Adiccionar</h1><br/>
+                                <input class="form-control me-auto" type="text" placeholder="Cantidad de producto a sumar al Stock:" aria-label="Add your item here..." />            
+                                <button type="button" class="btn btn-secondary">Submit</button>
+                                <div class="vr"></div>
+                                <button type="button" class="btn btn-outline-danger">Reset</button>
+                            </div>
+                           {/* 
                             <table className="table">
                                 <thead>
                                     <tr>
@@ -43,6 +51,8 @@ const Addstock = ({user}) => {
                                     </tr>
                                 </tbody>
                             </table>
+                            */}
+                    
                         </div>
                     </div>
             }</>
--- coldevs/src/Stock/Quickproduct.jsx
+++ coldevs/src/Stock/Quickproduct.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import '../App.css'
 import { useHistory } from 'react-router-dom'
 
-const Quickproduct = ({user}) => {
+const Quickproduct = ({ user }) => {
     return (
- 
+
         <div>
-            Aquí irá la pantalla para quitar la cantidad de producto.
+            <div class="hstack gap-3">
+              
+                <h1>Restar</h1><br />
+                <input class="form-control me-auto" type="text" placeholder="Cantidad de producto a restar del Stock:" aria-label="Add your item here..." />
+                <button type="button" class="btn btn-secondary">Submit</button>
+                <div class="vr"></div>
+                <button type="button" class="btn btn-outline-danger">Reset</button>
+
+            </div>
         </div>
     )
 }
