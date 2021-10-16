7913ad0ac6b1fd55a80179b6ed80990af257784e
Jose Fernando Sanchez Gomez <josefersango@gmail.com>
2021-10-15 23:57:07 -0500

    prueba

--- node/config/database.js
+++ node/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "Aletrox123",
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_DATABASE || "coldev",
   host: process.env.DB_HOST || "localhost",
   dialect: process.env.DB_DIALECT || "mysql",
